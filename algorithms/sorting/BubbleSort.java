0bb7b36ea1c270fe13cc13fa4ae5dcd992768aae
Akhil Guttula <akhil@stimveda.com>
2024-08-24 14:24:59 -0700

    fixing selection and bubble sort

--- algorithms/sorting/BubbleSort.java
+++ algorithms/sorting/BubbleSort.java
@@ -2,7 +2,6 @@ package algorithms.sorting;
 
 import algorithms.utils.Util;
 
-import java.util.Arrays;
 
 /**
  * repeatedly compare adjacent elements in array and rearrange accordingly.
@@ -10,11 +9,9 @@ import java.util.Arrays;
 public class BubbleSort implements Sorter {
     @Override
     public int[] sort(int[] arr) {
-        System.out.println(String
-                .format("Applying Bubble sort on %s", Arrays.toString(arr)));
         int n = arr.length;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n-1; j++) {
+            for (int j = 0; j < n-1-i; j++) {
                 if(arr[j] > arr[j+1]) {
                     Util.swap(arr, j, j+1);
                 }
--- algorithms/sorting/SelectionSort.java
+++ algorithms/sorting/SelectionSort.java
@@ -10,11 +10,13 @@ public class SelectionSort implements Sorter {
         System.out.println(String.format("Applying selection sort on %s", Arrays.toString(arr)));
         int n = arr.length;
         for (int i = 0; i < n; i++) {
+            int minIndex = i;
             for (int j = i+1; j < n; j++) {
-                if(arr[i] > arr[j]) {
-                    Util.swap(arr, i, j);
+                if(arr[minIndex] > arr[j]) {
+                    minIndex = j;
                 }
             }
+            Util.swap(arr, i, minIndex);
         }
         return arr;
     }
