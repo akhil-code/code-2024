b0bf8e79d636aa7dd556d0325d62f79d9548bdce
Akhil Guttula <akhil@stimveda.com>
2024-09-08 18:06:46 -0700

    Adding util function to print 2d array

--- datastructures/arrays/ArrUtil.java
+++ datastructures/arrays/ArrUtil.java
@@ -13,4 +13,15 @@ public class ArrUtil {
         }
         return arr;
     }
+
+    public static void print2DArray(int[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + ", ");
+            }
+            System.out.println("");
+        }
+    }
 }
